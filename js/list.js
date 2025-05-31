fec661508d9a60666a5076c7787b5232127d6949
theannoyinglist <nilzmail11@gmail.com>
2025-05-31 15:38:23 +0200

    Add files via upload

---
+++ js/list.js
@@ -0,0 +1,39 @@
+const d = {
+	"list": [
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "2o0",
+					"link": "recording corrupted",
+					"percent": "100",
+					"hz": "60Hz"
+				},
+
+      ],
+			"name": "The Sarcassion",
+			"author": "PinkGuyGMD",
+			"more": "none",
+			"id": "119967955",
+			"pass": "Free to copy",
+			"percentToQualify": "40%",
+			"verificationVid": "https://www.youtube.com/watch?v=m1HwqTttB1w",
+/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 0
+		},
+		/*=================================================================================*/
+
+
+
+
+/*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
+
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list;const version = d.version;
